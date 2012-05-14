c8b12a2542feb0a79839fa7b6b7eea635037af0d
Matthew Spence <msaspence@gmail.com>
2012-05-14 12:31:02 +0100

    First commit

---
+++ lib/tasks/organon_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :organon do
+#   # Task goes here
+# end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '8a45a7b4c49d9f8dbed7c4deb39544fe6db8f49503f8dbc017127f144a3cfaa9b9c87b44c61af876f952720da4ebbd48c0e0e74aeb7d61b0576c476779c650b4'
